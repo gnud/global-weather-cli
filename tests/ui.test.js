e7d7fcfcd27b0c778b7efd56296614f0d9175fe3
Damjan Dimitrioski <damjandimitrioski@gmail.com>
2020-11-16 22:51:46 +0100

    Added displaySingleRow test to test ui writing to STDOUT

---
+++ tests/ui.test.js
@@ -0,0 +1,30 @@
+describe('weatherAppUi', () => {
+    it('displaySingleRow', async () => {
+
+        const ui = require('../src/ui')
+        const enums = require('../src/enums')
+
+        const spy = jest.spyOn(console, 'log')
+        const mockData = [
+            'Nowhere',
+            enums.emptyData
+        ]
+
+        await ui.displaySingleRow(mockData)
+
+        expect(spy.mock.calls.slice(0, 4).join()). toContain([
+            [
+                "Data for : Nowhere"
+            ],
+            [
+                "\tTemp: null"
+            ],
+            [
+                "\tPressure: null"
+            ],
+            [
+                "\tHumidity: null"
+            ],
+        ].join())
+    })
+})
