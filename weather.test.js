286e76ee74ee7f31bd97741dc12570aeb0327985
Damjan Dimitrioski <damjandimitrioski@gmail.com>
2020-11-16 14:45:05 +0100

    Encapsulating the parseArgs in utils module
    - Updating tests to reflect parseArgs refactoring

--- weather.test.js
+++ weather.test.js
@@ -1,11 +1,11 @@
-const app = require('./weather')
-
 describe('weatherApp', () => {
     it('parseCliArguments', async () => {
+        const utils = require('./src/utils')
+
         const argsString = 'New York,10005,Tokyo,São Paulo,Pluto'
         const argsList = ['New York', '10005', 'Tokyo', 'São Paulo', 'Pluto']
         process.argv = [process.argv[0], process.argv[1], argsString]
-        const output = app.parseArgs()
+        const output = utils.parseArgs()
 
         expect(argsString).toEqual(output.join())
         expect(argsList).toEqual(expect.arrayContaining(output))
