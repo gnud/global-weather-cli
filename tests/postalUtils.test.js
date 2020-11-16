56dc1561e281ed6574d39ddc8c66c166e74789bd
Damjan Dimitrioski <damjandimitrioski@gmail.com>
2020-11-16 20:54:48 +0100

    Added lookupByPostal test to test converting postal code to city name

--- tests/postalUtils.test.js
+++ tests/postalUtils.test.js
@@ -8,4 +8,15 @@ describe('weatherAppPostalUtils', () => {
 
         expect(isPostal).toBeTruthy()
     })
+
+    it('lookupByPostal', async () => {
+        const postalApi = require('../src/postalApi')
+
+        const targetPostalCode = '1000' // Skopje, Macedonia
+        const targetCity = 'Skopje' // Skopje, Macedonia
+
+        const resultingCity = await postalApi.lookupByPostal(targetPostalCode)
+
+        expect(resultingCity).toEqual(targetCity)
+    })
 })
