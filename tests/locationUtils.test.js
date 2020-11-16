dec0cbf13ebf128b6f2e8f60c94cd50001a88bc2
Damjan Dimitrioski <damjandimitrioski@gmail.com>
2020-11-16 20:17:06 +0100

    Added weatherAppLocationUtils test to test if text input is a valid city

--- tests/locationUtils.test.js
+++ tests/locationUtils.test.js
@@ -1,3 +1,11 @@
 describe('weatherAppLocationUtils', () => {
+    it('checkCity', async () => {
+        const locationUtils = require('../src/locationUtils')
 
+        const location = 'Skopje' // Skopje, Macedonia
+
+        const isValid = locationUtils.checkCity(location)
+
+        expect(isValid).toBeTruthy()
+    })
 })
