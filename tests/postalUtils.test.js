7f6c550c1087104adbdd632894d2a6c876bebbae
Damjan Dimitrioski <damjandimitrioski@gmail.com>
2020-11-16 20:34:51 +0100

    Added canPostalBeUsed test to test weather given postal code is valid

--- tests/postalUtils.test.js
+++ tests/postalUtils.test.js
@@ -1,3 +1,11 @@
 describe('weatherAppPostalUtils', () => {
+    it('canPostalBeUsed', async () => {
+        const postalApi = require('../src/postalApi')
 
+        const targetPostalCode = '1000' // Skopje, Macedonia
+
+        const isPostal = postalApi.canPostalBeUsed(targetPostalCode)
+
+        expect(isPostal).toBeTruthy()
+    })
 })
