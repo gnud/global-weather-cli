9719b0f5a03611b335bc079c35a51701c3718c82
Damjan Dimitrioski <damjandimitrioski@gmail.com>
2020-11-16 19:47:14 +0100

    Implemented time2TzDiff code in the timeUtils

--- src/timeUtils.js
+++ src/timeUtils.js
@@ -4,4 +4,11 @@ function currentTime() {
     return moment()
 }
 
+function time2TzDiff(momentTime, diffSeconds) {
+    return momentTime
+        .utc()
+        .add(diffSeconds, 'seconds')
+}
+
 module.exports.currentTime = currentTime
+module.exports.time2TzDiff = time2TzDiff
