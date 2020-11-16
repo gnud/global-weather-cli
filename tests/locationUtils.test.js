db37b8a1f4efd5794fceeadcffafd0bb0cb3a59b
Damjan Dimitrioski <damjandimitrioski@gmail.com>
2020-11-16 21:11:59 +0100

    Added validCityByPostal test to test whether calling validCity will provide a city as output

--- tests/locationUtils.test.js
+++ tests/locationUtils.test.js
@@ -8,4 +8,15 @@ describe('weatherAppLocationUtils', () => {
 
         expect(isValid).toBeTruthy()
     })
+
+    it('validCityByPostal', async () => {
+        const locationUtils = require('../src/locationUtils')
+
+        const location = '1000' // Skopje, Macedonia
+        const targetLocation = 'Skopje' // Skopje, Macedonia
+
+        const cityTarget = await locationUtils.validCity(location)
+
+        expect(cityTarget.city).toEqual(targetLocation)
+    })
 })
